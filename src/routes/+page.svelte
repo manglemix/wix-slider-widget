b5a45bdd59420f8226698ea828d700965b5fc2ff
Najman <najman.husaini@utah.edu>
2024-03-22 00:17:38 -0600

    draft 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,67 @@
     import { page } from '$app/stores';
 	import { beforeUpdate } from 'svelte';
     
-    let leftUrl: string; 
-    let rightUrl: string;
+    let width = 500;
+
+    let leftUrl: string | null; 
+    let rightUrl: string | null;
+
     beforeUpdate(() => {
         leftUrl = $page.url.searchParams.get('left');
         rightUrl = $page.url.searchParams.get('right');
+
+        if ($page.url.searchParams.has("width")) {
+            width = Number($page.url.searchParams.get("width"));
+        }
     });
 
-    let slider = '50%';
+    let slider = 50.0;
+    let img: HTMLImageElement;
 </script>
 
 <div>
-    <img src={leftUrl} alt="left" class="pane left" style="--slider: {slider}">
-    <img src={rightUrl} alt="right" class="pane right" style="--slider: {slider}">
+    <img src={leftUrl} alt="left" class="pane" bind:this={img} style="--width: {width}px">
+    <img src={rightUrl} alt="right" class="pane right" style="--slider: {slider}%; --width: {width}px">
+    <div id="slider-column" style="--height: {img?.height ?? 0}px; --left: {slider / 100 * width + 6}px;"></div>
+    <div id="sliderPanel" style="--height: {img?.height ?? 0}px; --width: {width}px;">
+        <input type="range" bind:value={slider} step="any">
+    </div>
 </div>
 
 <style>
-    .pane {
-        width: 300px;
-        position: absolute;
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    #sliderPanel {
+        display: flex;
+        flex-direction: column;
+        width: var(--width);
+        height: var(--height);
+        justify-content: center;
     }
 
-    .left {
-        clip-path: rect(0 var(--slider) 100% 0);
+    #sliderPanel > input {
+        margin-left: -7px;
+        margin-right: -7px;
+        z-index: 100;
+        appearance: none;
+        background: transparent;
+        clip-path: rect(0 calc(100% - 7px) 100% 7px);
+    }
+
+    #slider-column {
+        position: absolute;
+        height: var(--height);
+        width: 3px;
+        left: var(--left);
+        z-index: 100;
+        background-color: white;
+    }
+
+    .pane {
+        width: var(--width);
+        position: absolute;
     }
 
     .right {
