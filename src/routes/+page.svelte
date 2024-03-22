88a7a75bbbac45c3662fbef0701fbfd465be1cca
Najman <najman.husaini@utah.edu>
2024-03-21 23:20:20 -0600

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+    let slider = '50%';
+</script>
+
+<div>
+    <img src="/STILL2.jpg" alt="left" class="pane left" style="--slider: {slider}">
+    <img src="/STILL1.jpg" alt="right" class="pane right" style="--slider: {slider}">
+</div>
+
+<style>
+    .pane {
+        width: 300px;
+        position: absolute;
+    }
+
+    .left {
+        clip-path: rect(0 var(--slider) 100% 0);
+    }
+
+    .right {
+        clip-path: rect(0 100% 100% var(--slider));
+    }
+</style>
