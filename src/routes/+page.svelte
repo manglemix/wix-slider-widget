bebe84dc749efa77bf12be5d2940b14a2aaec942
Najman <najman.husaini@utah.edu>
2024-03-22 00:42:42 -0600

    draft 1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,71 +1,121 @@
-<script lang=ts>
-    import { page } from '$app/stores';
+<script lang="ts">
+	import { page } from '$app/stores';
 	import { beforeUpdate } from 'svelte';
-    
-    let width = 500;
 
-    let leftUrl: string | null; 
-    let rightUrl: string | null;
+	let width = 500;
+	let slider = 50.0;
 
-    beforeUpdate(() => {
-        leftUrl = $page.url.searchParams.get('left');
-        rightUrl = $page.url.searchParams.get('right');
+	let leftUrl: string | null;
+	let rightUrl: string | null;
 
-        if ($page.url.searchParams.has("width")) {
-            width = Number($page.url.searchParams.get("width"));
-        }
-    });
+	beforeUpdate(() => {
+		leftUrl = $page.url.searchParams.get('left');
+		rightUrl = $page.url.searchParams.get('right');
 
-    let slider = 50.0;
-    let img: HTMLImageElement;
+		if ($page.url.searchParams.has('width')) {
+			width = Number($page.url.searchParams.get('width'));
+		}
+
+		if ($page.url.searchParams.has('slider')) {
+			slider = Number($page.url.searchParams.get('slider'));
+		}
+	});
+
+	let img: HTMLImageElement;
 </script>
 
 <div>
-    <img src={leftUrl} alt="left" class="pane" bind:this={img} style="--width: {width}px">
-    <img src={rightUrl} alt="right" class="pane right" style="--slider: {slider}%; --width: {width}px">
-    <div id="slider-column" style="--height: {img?.height ?? 0}px; --left: {slider / 100 * width + 6}px;"></div>
-    <div id="sliderPanel" style="--height: {img?.height ?? 0}px; --width: {width}px;">
-        <input type="range" bind:value={slider} step="any">
-    </div>
+	<img src={leftUrl} alt="left" class="pane" bind:this={img} style="--width: {width}px" on:load={() => img = img}/>
+	<img
+		src={rightUrl}
+		alt="right"
+		class="pane right"
+		style="--slider: {slider}%; --width: {width}px"
+	/>
+	<div
+		id="slider-column"
+		style="--height: {img?.height ?? 0}px; --left: {(slider / 100) * width + 7}px;"
+	></div>
+	<div id="sliderPanel" style="--height: {img?.height ?? 0}px; --width: {width}px;">
+		<input type="range" bind:value={slider} step="any" />
+	</div>
 </div>
 
 <style>
-    * {
-        margin: 0;
-        padding: 0;
-    }
-    #sliderPanel {
-        display: flex;
-        flex-direction: column;
-        width: var(--width);
-        height: var(--height);
-        justify-content: center;
-    }
+	:global(body) {
+		background-color: black;
+	}
 
-    #sliderPanel > input {
-        margin-left: -7px;
-        margin-right: -7px;
-        z-index: 100;
+	* {
+		margin: 0;
+		padding: 0;
+	}
+
+	#sliderPanel {
+		display: flex;
+		flex-direction: column;
+		width: var(--width);
+		height: var(--height);
+		justify-content: center;
+	}
+
+	#sliderPanel > input {
+		margin-left: -7px;
+		margin-right: -7px;
+		z-index: 100;
+		appearance: none;
+		background: transparent;
+		clip-path: rect(0 calc(100% - 7px) 100% 6px);
+	}
+
+	#slider-column {
+		position: absolute;
+		height: var(--height);
+		width: 1px;
+		left: var(--left);
+		z-index: 100;
+		background-color: white;
+	}
+
+	.pane {
+		width: var(--width);
+		position: absolute;
+	}
+
+	.right {
+		clip-path: rect(0 100% 100% var(--slider));
+	}
+
+	/* For slider */
+	input[type='range'] {
+		-webkit-appearance: none;
         appearance: none;
-        background: transparent;
-        clip-path: rect(0 calc(100% - 7px) 100% 7px);
-    }
+	}
+	input[type='range']::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		border: none;
+		height: 16px;
+		width: 16px;
+		border-radius: 50%;
+		background: white;
+	}
+	input[type='range']:focus {
+		outline: none;
+	}
+	input[type='range']::-moz-range-thumb {
+		border: none;
+		height: 16px;
+		width: 16px;
+		border-radius: 50%;
+		background: white;
+	}
 
-    #slider-column {
-        position: absolute;
-        height: var(--height);
-        width: 3px;
-        left: var(--left);
-        z-index: 100;
-        background-color: white;
-    }
-
-    .pane {
-        width: var(--width);
-        position: absolute;
-    }
-
-    .right {
-        clip-path: rect(0 100% 100% var(--slider));
-    }
+	/* for ie */
+	input[type='range']::-ms-thumb {
+		border: none;
+		height: 16px;
+		width: 16px;
+		border-radius: 50%;
+		background: white;
+	}
 </style>
